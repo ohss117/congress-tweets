d9e3012ff03e57a95c6448f9cda88af9d8e9dd30
ohss117 <ohss@hendrix.edu>
2013-09-25 14:26:45 -0500

    wrapped api authentication. added archival functionality. todo: sort out congress members

--- main/dawgs.py
+++ main/dawgs.py
@@ -3,16 +3,14 @@ Created on Sep 16, 2013
 
 @author: sungoh
 '''
-import tweepy, os
+import tweepy
+from auth import ApiAuthorize
 
-#Stored keys/secrets as environment variables
-auth = tweepy.auth.OAuthHandler(os.environ.get('CONSUMERTOKEN'), os.environ.get('CONSUMERSECRET'))
-auth.set_access_token(os.environ.get('ACCESSTOKEN'), os.environ.get('ACCESSSECRET'))
-api = tweepy.API(auth)
-
-
+api = ApiAuthorize.api
+    
+#Testing API        
 if api.verify_credentials() is not False:
-    print 'good'
+        print 'good'
 else:
     print 'Invalid Authentication'
  
@@ -21,7 +19,7 @@ print api.get_user(screen_name = 'NancyPelosi').__getstate__()
 barack = api.get_user(screen_name = 'BarackObama').__getstate__()
 
 print barack['screen_name']
-
+    
 #All the members of the congress that have twitter accounts according to this list-- https://twitter.com/cspan/lists/members-of-congress
 dawgs = []
 for member in tweepy.Cursor(api.list_members, 'CSPAN', 'members-of-congress').items():
